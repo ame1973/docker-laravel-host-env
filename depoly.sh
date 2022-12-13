560a1c12282c70eed17c3b93e6ef03039bde0137
HuanXiang <qq418610675@gmail.com>
2022-12-13 18:08:06 +0800

    update

--- depoly.sh
+++ depoly.sh
@@ -11,16 +11,16 @@ fi
 
 cp docker-compose.example docker-compose.yml
 
-sed -i '' "s/YOUR_PROJECT_NAME/$projectName/g" docker-compose.yml
-sed -i '' "s/YOUR_PROJECT_DOMAIN/$projectDomain/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_NAME/$projectName/g" docker-compose.yml
+sed -i "s/YOUR_PROJECT_DOMAIN/$projectDomain/g" docker-compose.yml
 
 DEFAULT="n"
 read -p "Run Laravel at octane? [y/N]: " eOctane
 eOctane="${eOctane:-${DEFAULT}}"
 if [ "${eOctane}" = "y" ] || [ "${eOctane}" = "Y" ]; then
-	sed -i '' "s/#laravel-octane//g" docker-compose.yml
+	sed -i "s/#laravel-octane//g" docker-compose.yml
 else
-	sed -i '' "s/#php-fpm//g" docker-compose.yml
+	sed -i "s/#php-fpm//g" docker-compose.yml
 fi
 
 MYSQL_COMMAND="create database ${projectName}_db;"
@@ -30,9 +30,9 @@ read -p "Enable Frontend web service? [y/N]: " eFrontend
 eFrontend="${eFrontend:-${DEFAULT}}"
 if [ "${eFrontend}" = "y" ] || [ "${eFrontend}" = "Y" ] ; then
   read -p 'Frontend Domain: ' frontendDomain
-  sed -i '' "s/YOUR_PROJECT_FRONTEND_DOMAIN/$frontendDomain/g" docker-compose.yml
+  sed -i "s/YOUR_PROJECT_FRONTEND_DOMAIN/$frontendDomain/g" docker-compose.yml
 
-	sed -i '' "s/#frontend-service//g" docker-compose.yml
+	sed -i "s/#frontend-service//g" docker-compose.yml
 	if [ ! -d "frontend_src" ]; then
     	mkdir "frontend_src"
   fi
@@ -42,7 +42,7 @@ read -p "Enable Web3/Node service? [y/N]: " eFrontend
 eFrontend="${eFrontend:-${DEFAULT}}"
 if [ "${eFrontend}" = "y" ] || [ "${eFrontend}" = "Y" ] ; then
 
-	sed -i '' "s/#web3-service//g" docker-compose.yml
+	sed -i "s/#web3-service//g" docker-compose.yml
 	if [ ! -d "web3_src" ]; then
     	mkdir "web3_src"
   fi
@@ -63,9 +63,9 @@ fi
 #fi
 
 cp ./backup/backup_db.sh.example backup_db.sh
-sed -i '' "s/YOUR_PROJECT_NAME/$projectName/g" backup_db.sh
+sed -i "s/YOUR_PROJECT_NAME/$projectName/g" backup_db.sh
 
 cp ./config/script/bash.sh.example bash.sh
-sed -i '' "s/YOUR_PROJECT_NAME/$projectName/g" bash.sh
+sed -i "s/YOUR_PROJECT_NAME/$projectName/g" bash.sh
 
 echo "Done."
